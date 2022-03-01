12d1596aa137760c57d8dfd849ba78c11c77c414
Crowdbotics <team@crowdbotics.com>
2022-03-01 05:18:49 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/casual_dating_app_33915/MainActivity.java
@@ -0,0 +1,15 @@
+package com.casual_dating_app_33915;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "casual_dating_app_33915";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "casual_dating_app_33915",
+  url: "https://casual_dating_app_33915.botics.co",
+  api: "https://casual_dating_app_33915.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
